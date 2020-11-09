1b3a0700ccf1d38deb40559fedbf1889cb387e49
IvanovVladlen <vlad11337799!>
2020-11-09 09:02:24 +0700

    Range*

---
+++ Range/src/ru/academits/ivanov/main/Main.java
@@ -0,0 +1,55 @@
+package ru.academits.ivanov.main;
+
+import ru.academits.ivanov.range.Range;
+
+public class Main {
+    private static void getOperationsWithRangesResults(Range rangeA, Range rangeB) {
+        Range intersectionRanges = rangeA.getIntersection(rangeB);
+        System.out.println("Интервал пересечения двух интервалов: " + intersectionRanges);
+
+        Range[] unionRanges = rangeA.getUnion(rangeB);
+        System.out.print("Интервал(ы) объединения двух интервалов: ");
+        for (Range e : unionRanges) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+
+        Range[] differenceRanges = rangeA.getDifference(rangeB);
+
+        if (differenceRanges.length < 1) {
+            System.out.println("Интервал полностью вычтен.");
+        } else {
+            System.out.print("Интервал(ы) разности двух интервалов: ");
+            for (Range e : differenceRanges) {
+                System.out.print(e + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Range range = new Range(5, 16);
+        System.out.println("Дан диапазон " + range + ": ");
+
+        double rangeLength = range.getLength();
+        System.out.println("Длина заданного диапазона равна: " + rangeLength);
+
+        double checkNumber = 10.5;
+        boolean isInsideCheckNumber = range.isInside(checkNumber);
+
+        if (isInsideCheckNumber) {
+            System.out.println("Число " + checkNumber + " входит в диапазон заданых чисел.");
+        } else {
+            System.out.println("Число " + checkNumber + " не входит в диапазон заданых чисел.");
+        }
+
+        System.out.println("----------------------------------------------------------");
+
+        Range rangeA = new ru.academits.ivanov.range.Range(4, 28);
+        Range rangeB = new ru.academits.ivanov.range.Range(10, 30);
+        System.out.println("Проведем ряд математических операций для диапазонов: " + rangeA + " и "
+                + rangeB + ".");
+
+        System.out.println("Результат:");
+        getOperationsWithRangesResults(rangeA, rangeB);
+    }
+}
---
+++ Range/src/ru/academits/ivanov/range/Range.java
@@ -0,0 +1,80 @@
+package ru.academits.ivanov.range;
+
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double x) {
+        return x >= from && x <= to;
+    }
+
+    public Range getIntersection(Range range) {
+        if (from >= range.to || range.from >= to) {
+            return null;
+        }
+
+        return new Range(Math.max(from, range.from), Math.min(to, range.to));
+    }
+
+    public Range[] getUnion(Range range) {
+        if (to < range.from) {
+            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
+        }
+
+        if (from > range.to) {
+            return new Range[]{new Range(range.from, range.to), new Range(from, to)};
+        }
+
+        return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (to <= range.from || from >= range.to) {
+            return new Range[]{new Range(from, to)};
+        }
+
+        if (from >= range.from && to <= range.to) {
+            return new Range[]{};
+        }
+
+        if (from < range.from && to > range.to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        }
+
+        if (from >= range.from) {
+            return new Range[]{new Range(range.to, to)};
+        }
+
+        return new Range[]{new Range(from, range.from)};
+    }
+
+    @Override
+    public String toString() {
+        return "[" + from + " : " + to + "]";
+    }
+}
